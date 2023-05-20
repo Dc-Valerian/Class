af5b35a07bcddf145c0493add4b4299b1ecd6604
Dc Valerian <Dc-Valerian>
2023-05-20 12:03:56 +0100

    u

--- src/Components/Block/Header.tsx
+++ src/Components/Block/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div>
-      valerian
+     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod in a vel, maxime asperiores eligendi officia exercitationem quae necessitatibus explicabo sequi itaque voluptate quo ratione velit expedita. Odit, voluptas corporis!
     </div>
   );
 }
